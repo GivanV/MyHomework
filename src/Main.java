dc2de1569361e2ceae9786883850e4aeb1cea166
Ivan <ivan-1997@bk.ru>
2023-05-24 19:37:46 +0300

    sout for brunoyam

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.printf("Hello world");
         System.out.printf("Hello Max");
+        System.out.printf("Hello brunoyam");
 
     }
 }
